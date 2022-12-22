f2b11b4d3b179e10b95f87dc7d6c8e35637dc03c
A-baji <bidabaji@gmail.com>
2022-12-21 20:18:12 -0600

    first commit

---
+++ create_model.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# sh create_model.sh <discord-logs.json> <"discord_user#id"> <base_model=ada|babbage|curie|davinci>
+
+python3.11 data_set_gen.py $1 "$2"
+
+rm "$2_data_set_prepared.jsonl"
+openai tools fine_tunes.prepare_data -f "$2_data_set.jsonl"
+openai api fine_tunes.create -t "$2_data_set_prepared.jsonl" -m $3 --suffix "$2"
+rm "$2_data_set.jsonl"
+rm "$2_data_set_prepared.jsonl"
