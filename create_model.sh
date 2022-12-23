797213b65e0618ce9daefc6face95d528c1034cf
A-Baji <bidabaji@gmail.com>
2022-12-22 20:04:33 -0600

    add condition to model creation command

--- create_model.sh
+++ create_model.sh
@@ -5,7 +5,10 @@ python3.11 data_set_gen.py $1 "$2"
 
 rm "$2_data_set_prepared.jsonl"
 openai tools fine_tunes.prepare_data -f "$2_data_set.jsonl"
-openai api fine_tunes.create -t "$2_data_set_prepared.jsonl" -m $3 --suffix "$2"
+if [ $3 == "davinci" ] || [ $3 == "curie" ] || [ $3 == "babbage" ] || [ $3 == "ada" ]
+then
+    openai api fine_tunes.create -t "$2_data_set_prepared.jsonl" -m $3 --suffix "$2"
+fi
 
 if [ $4 == "clean" ]
 then
