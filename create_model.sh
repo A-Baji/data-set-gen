73df68c26396f428431a20d719731a335e6640a1
A-Baji <bidabaji@gmail.com>
2022-12-24 16:23:04 -0600

    add discord chat export to script

--- create_model.sh
+++ create_model.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
-# sh create_model.sh <discord-logs.json> <"discord_user#id"> <base_model=ada|babbage|curie|davinci>
 
+# Usage:
+# export DISCORD_TOKEN=<your discord bot token>
+# export OPENAI_API_KEY=<your openAI api key>
+# bash create_model.sh <channel id> <"discord_user#id"> <ada|babbage|curie|davinci|none <- skips model building> <clean|dirty <- keeps generated files>
+
+echo "INFO: Exporting chat logs..."
+echo "INFO: This may take a few minutes to hours depending on the size of the discord channel"
+echo "INFO: Progress will NOT be saved if canceled"
+./DiscordChatExporter/DiscordChatExporter.Cli.exe export -c $1 -t $DISCORD_TOKEN -o "$1_$2_logs.json" -f Json --filter "from:$2"
+
+echo "INFO: Parsing chat logs into a openAI compatible dataset..."
 python3.11 data_set_gen.py $1 "$2"
 
+echo "INFO: Cleaning up generated dataset..."
 rm "$1_$2_data_set_prepared.jsonl"
-openai tools fine_tunes.prepare_data -f "$1_$2_data_set.jsonl"
+openai tools fine_tunes.prepare_data -f "$1_$2_data_set.jsonl" -q
+
 if [ $3 == "davinci" ] || [ $3 == "curie" ] || [ $3 == "babbage" ] || [ $3 == "ada" ]
 then
-    openai api fine_tunes.create -t "$1_$2_data_set_prepared.jsonl" -m $3 --suffix "$2"
+    echo "INFO: Training customized openAI model..."
+    echo "INFO: This may take a few minutes to hours depending on the size of the dataset and the selected base model"
+    if [ -f "$1_$2_data_set_prepared.jsonl" ]
+    then
+        openai api fine_tunes.create -t "$1_$2_data_set_prepared.jsonl" -m $3 --suffix "$2"
+    else
+        openai api fine_tunes.create -t "$1_$2_data_set.jsonl" -m $3 --suffix "$2"
+    fi
+else
+    echo "No base model selected... Skipping training"
 fi
 
 if [ $4 == "clean" ]
 then
+    rm "$1_$2_logs.json"
     rm "$1_$2_data_set.jsonl"
     rm "$1_$2_data_set_prepared.jsonl"
 fi
--- data_set_gen.py
+++ data_set_gen.py
@@ -4,10 +4,10 @@ import sys
 import re
 
 
-def gen_data_set(file, user, thought_time=10000):
-    dataset = open(f'{file}_{user}_data_set.jsonl', 'w')
+def gen_data_set(channel, user, thought_time=10000):
+    dataset = open(f'{sys.argv[1]}_{user}_data_set.jsonl', 'w')
     user_dat = user.split('#')
-    with open(file, 'r', encoding='utf-8') as data_file:
+    with open(f'{channel}_{user}_logs.json', 'r', encoding='utf-8') as data_file:
         data = json.load(data_file)
         messages = [msg for msg in data['messages']
                     if msg['author']['name'] == user_dat[0] and msg['author']['discriminator'] == user_dat[1]]
@@ -17,7 +17,8 @@ def gen_data_set(file, user, thought_time=10000):
                 r'\b(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~|!:,.;]+[-A-Za-z0-9+&@#/%=~_|?.]+[-A-Za-z0-9+&@#/%=~_|?]', '', msg['content'])
             if msg['content']:
                 if i == 0:
-                    thought = msg['content']
+                    thought = msg['content'] if msg['content'][
+                        0] == ' ' else f" {msg['content']}"
                 if i > 0:
                     prev_timestamp = datetime.datetime.fromisoformat(
                         messages[i-1]['timestamp'])
@@ -28,14 +29,15 @@ def gen_data_set(file, user, thought_time=10000):
                     if differentiation > thought_time:  # If time between messages exceed `thought_time` milliseconds
                         if len(thought.split(" ")) > 3:  # If the thought has more than three words
                             dataset.write(json.dumps(
-                                {'prompt': '', 'completion': thought if thought[-1] == '.' else thought + '.'}) + "\n")
-                        thought = msg['content']
+                                {'prompt': '', 'completion': f'{thought}###' if thought[-1] == '.' else f'{thought}.###'}) + "\n")
+                        thought = msg['content'] if msg['content'][
+                            0] == ' ' else f" {msg['content']}"
                     else:
                         thought += f" {msg['content']}"
                     # If it is the last message and the thought has more than three words
                     if i == len(messages)-1 and len(thought.split(" ")) > 3:
                         dataset.write(json.dumps(
-                            {'prompt': '', 'completion': thought if thought[-1] == '.' else thought + '.'}) + "\n")
+                            {'prompt': '', 'completion': f'{thought}###' if thought[-1] == '.' else f'{thought}.###'}) + "\n")
     dataset.close()
 
 
