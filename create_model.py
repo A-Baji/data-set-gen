cc496eb8fa63a731515095bf0d50f1b48420cb31
A-Baji <bidabaji@gmail.com>
2022-12-31 18:08:15 -0600

    convert to function

--- create_model.py
+++ create_model.py
@@ -1,96 +1,82 @@
 import os
-import sys
 import subprocess
 
-from gen_dataset import parse_logs
+from gen_dataset import parse_logs, get_lines
 
-# Usage:
-# python create_model.py <channel id> <"discord user#id"> <max dataset entry count=1000>
-#                        <reduce_mode:first|last|middle|even=even> <ada|babbage|curie|davinci|none <- skips model building>
-#                        <clean|dirty <- keeps generated files> <discord bot token> <openai api key>
 
-os.environ["OPENAI_API_KEY"] = sys.argv[8]
+def create_model(bot_token: str, openai_key: str, channel_id: str, user_id: str, thought_time=10000,
+                 max_entry_count=1000, reduce_mode="even", base_model="none", clean=True):
+    os.environ["OPENAI_API_KEY"] = openai_key
+    channel_user = f"{channel_id}_{user_id}"
 
-# Download logs
-if not os.path.isfile(f"{sys.argv[1]}_{sys.argv[2]}_logs.json"):
-    print("INFO: Exporting chat logs using DiscordChatExporter...")
-    print("INFO: This may take a few minutes to hours depending on the message count of the channel")
-    print("INFO: Progress will NOT be saved if cancelled")
-    print("---------------DiscordChatExporter---------------")
-    subprocess.run([
-        "./DiscordChatExporter/DiscordChatExporter.Cli.exe",
-        "export",
-        "-c", sys.argv[1],
-        "-t", sys.argv[7],
-        "-o", f"{sys.argv[1]}_{sys.argv[2]}_logs.json",
-        "-f", "Json",
-        "--filter", f"from:'{sys.argv[2]}'"
-    ])
-    print("---------------DiscordChatExporter---------------")
-else:
-    print("INFO: Chat logs detected locally... Skipping download.")
-
-# Parse logs
-if not os.path.isfile(f"{sys.argv[1]}_{sys.argv[2]}_data_set.jsonl"):
-    print("INFO: Parsing chat logs into a openAI compatible dataset...")
-    parse_logs(f"{sys.argv[1]}_{sys.argv[2]}_logs.json", sys.argv[2])
-    try:
-        os.remove(f"{sys.argv[1]}_{sys.argv[2]}_data_set_prepared.jsonl")
-    except FileNotFoundError:
-        pass
-else:
-    print("INFO: Dataset detected locally... Skipping parsing.")
-
-# Prepare and reduce dataset
-if not os.path.isfile(f"{sys.argv[1]}_{sys.argv[2]}_data_set_prepared.jsonl"):
-    print("INFO: Cleaning up generated dataset...")
-    subprocess.run([
-        "openai", "tools", "fine_tunes.prepare_data",
-        "-f", f"{sys.argv[1]}_{sys.argv[2]}_data_set.jsonl",
-        "-q"
-    ])
-    if os.path.isfile(f"{sys.argv[1]}_{sys.argv[2]}_data_set_prepared.jsonl"):
+    # Download logs
+    if not os.path.isfile(f"{channel_user}_logs.json"):
+        print("INFO: Exporting chat logs using DiscordChatExporter...")
+        print("INFO: This may take a few minutes to hours depending on the message count of the channel")
+        print("INFO: Progress will NOT be saved if cancelled")
+        print("---------------DiscordChatExporter---------------")
         subprocess.run([
-            "python3.11",
-            "reduce_dataset.py",
-            f"{sys.argv[1]}_{sys.argv[2]}_data_set_prepared.jsonl",
-            sys.argv[3],
-            sys.argv[4]
+            "./DiscordChatExporter/DiscordChatExporter.Cli.exe",
+            "export",
+            "-c", channel_id,
+            "-t", bot_token,
+            "-o", f"{channel_user}_logs.json",
+            "-f", "Json",
+            "--filter", f"from:'{user_id}'"
         ])
+        print("---------------DiscordChatExporter---------------")
     else:
-        subprocess.run([
-            "python3.11",
-            "reduce_dataset.py",
-            f"{sys.argv[1]}_{sys.argv[2]}_data_set.jsonl",
-            sys.argv[3],
-            sys.argv[4]
-        ])
+        print("INFO: Chat logs detected locally... Skipping download.")
 
-# Train customized openAI model
-if sys.argv[5] in ["davinci", "curie", "babbage", "ada"]:
-    print("INFO: Training customized openAI model...")
-    print("INFO: This may take a few minutes to hours depending on the size of the dataset and the selected base model")
-    if os.path.isfile(f"{sys.argv[1]}_{sys.argv[2]}_data_set_prepared.jsonl"):
-        subprocess.run([
-            "openai", "api", "fine_tunes.create",
-            "-t", f"{sys.argv[1]}_{sys.argv[2]}_data_set_prepared.jsonl",
-            "-m", sys.argv[5],
-            "--suffix", sys.argv[2]
-        ])
+    # Parse logs
+    if not os.path.isfile(f"{channel_user}_data_set.jsonl"):
+        print("INFO: Parsing chat logs into a openAI compatible dataset...")
+        parse_logs(f"{channel_user}_logs.json", user_id, thought_time)
+        try:
+            os.remove(f"{channel_user}_data_set_prepared.jsonl")
+        except FileNotFoundError:
+            pass
     else:
-        subprocess.run([
-            "openai", "api", "fine_tunes.create",
-            "-t", f"{sys.argv[1]}_{sys.argv[2]}_data_set.jsonl",
-            "-m", sys.argv[5],
-            "--suffix", sys.argv[2]
-        ])
-else:
-    print("INFO: No base model selected... Skipping training.")
+        print("INFO: Dataset detected locally... Skipping parsing.")
 
-# Cleaning up generated files
-if sys.argv[6] == "clean":
-    try:
-        os.remove(f"{sys.argv[1]}_{sys.argv[2]}_data_set.jsonl")
-        os.remove(f"{sys.argv[1]}_{sys.argv[2]}_data_set_prepared.jsonl")
-    except FileNotFoundError:
-        pass
+    # Prepare and reduce dataset
+    if not os.path.isfile(f"{channel_user}_data_set_prepared.jsonl"):
+        print("INFO: Cleaning up generated dataset...")
+        subprocess.run([
+            "openai", "tools", "fine_tunes.prepare_data",
+            "-f", f"{channel_user}_data_set.jsonl",
+            "-q"
+        ])
+        if os.path.isfile(f"{channel_user}_data_set_prepared.jsonl"):
+            get_lines(f"{channel_user}_data_set_prepared.jsonl", max_entry_count, reduce_mode)
+        else:
+            get_lines(f"{channel_user}_data_set.jsonl", max_entry_count, reduce_mode)
+
+    # Train customized openAI model
+    if base_model in ["davinci", "curie", "babbage", "ada"]:
+        print("INFO: Training customized openAI model...")
+        print("INFO: This may take a few minutes to hours depending on the size of the dataset and the selected base model")
+        if os.path.isfile(f"{channel_user}_data_set_prepared.jsonl"):
+            subprocess.run([
+                "openai", "api", "fine_tunes.create",
+                "-t", f"{channel_user}_data_set_prepared.jsonl",
+                "-m", base_model,
+                "--suffix", user_id
+            ])
+        else:
+            subprocess.run([
+                "openai", "api", "fine_tunes.create",
+                "-t", f"{channel_user}_data_set.jsonl",
+                "-m", base_model,
+                "--suffix", user_id
+            ])
+    else:
+        print("INFO: No base model selected... Skipping training.")
+
+    # Cleaning up generated files
+    if clean:
+        try:
+            os.remove(f"{channel_user}_data_set.jsonl")
+            os.remove(f"{channel_user}_data_set_prepared.jsonl")
+        except FileNotFoundError:
+            pass
--- gen_dataset.py
+++ gen_dataset.py
@@ -1,10 +1,9 @@
 import json
 import datetime
-import sys
 import re
 
 
-def parse_logs(file, user, thought_time=10000):
+def parse_logs(file: str, user: str, thought_time=10000):
     dataset = open(
         f"{file.split('_')[0]}_{user}_data_set.jsonl", 'w')
     with open(file, 'r', encoding='utf-8') as data_file:
@@ -46,5 +45,41 @@ def parse_logs(file, user, thought_time=10000):
     dataset.close()
 
 
-if __name__ == '__main__':
-    parse_logs(sys.argv[1], sys.argv[2])
+def get_lines(file_name, N, method):
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+    num_lines = len(lines)
+
+    if N > num_lines:
+        return
+
+    if method == 'first':
+        selected_lines = lines[:N]
+    elif method == 'last':
+        selected_lines = lines[-N:]
+    elif method == 'middle':
+        start = num_lines // 2 - N // 2
+        end = start + N
+        selected_lines = lines[start:end]
+    else:
+        step = 5
+        start = 0
+        selected_lines = lines[start::step][:N]
+        while len(selected_lines) < N:
+            middle = num_lines // 2
+            start = middle - step // 2
+            if start in selected_lines:
+                start += 1
+            selected_lines += lines[start::step]
+            selected_lines = list(set(selected_lines))
+        while len(selected_lines) < N:
+            middle = num_lines // 2
+            start = middle - step // 2
+            if start in selected_lines:
+                start += 1
+            selected_lines += lines[start-step:start+step+1:step]
+            selected_lines = list(set(selected_lines))
+        selected_lines = selected_lines[:N]
+
+    with open(file_name, "w") as f:
+        f.writelines(selected_lines)
