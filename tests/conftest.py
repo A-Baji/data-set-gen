dc0742b1a659bd538121ec2690afaebed0c28763
A-Baji <bidabaji@gmail.com>
2024-06-28 21:30:06 -0500

    add conftest

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+import os
+import pathlib
+import appdirs
+import pytest
+
+from discordai_modelizer import customize
+
+
+CHANNEL_ID = os.environ["CHANNEL_ID"]
+USER = os.environ["USERNAME"]
+FILES_PATH = pathlib.Path(appdirs.user_data_dir(appname="discordai"))
+FULL_LOGS_PATH = FILES_PATH / f"{CHANNEL_ID}_logs.json"
+FULL_DATASET_PATH = FILES_PATH / f"{CHANNEL_ID[:4]}_{USER}_data_set.jsonl"
+
+
+def list_dict_comp(x, y):
+    assert len(x) == len(y)
+    for d1, d2 in zip(x, y):
+        assert d1 == d2
+
+
+@pytest.fixture(scope="session")
+def default_file_output():
+    # Ensure a clean start by removing any existing files
+    if FULL_LOGS_PATH.exists():
+        FULL_LOGS_PATH.unlink()
+    if FULL_DATASET_PATH.exists():
+        FULL_DATASET_PATH.unlink()
+    # Generate log and dataset files
+    customize.create_model(CHANNEL_ID, USER)
+    yield
+    # Cleanup after the tests in this module
+    if FULL_LOGS_PATH.exists():
+        FULL_LOGS_PATH.unlink()
+    if FULL_DATASET_PATH.exists():
+        FULL_DATASET_PATH.unlink()
--- tests/test_modelization.py
+++ tests/test_modelization.py
@@ -8,29 +8,7 @@ from json import load
 from discordai_modelizer import customize
 from discordai_modelizer.openai import set_openai_api_key
 from . import expected_values
-
-CHANNEL_ID = os.environ["CHANNEL_ID"]
-USER = os.environ["USERNAME"]
-FILES_PATH = pathlib.Path(appdirs.user_data_dir(appname="discordai"))
-FULL_LOGS_PATH = FILES_PATH / f"{CHANNEL_ID}_logs.json"
-FULL_DATASET_PATH = FILES_PATH / f"{CHANNEL_ID[:4]}_{USER}_data_set.jsonl"
-
-
-@pytest.fixture(scope="module")
-def default_file_output():
-    # Ensure a clean start by removing any existing files
-    if FULL_LOGS_PATH.exists():
-        FULL_LOGS_PATH.unlink()
-    if FULL_DATASET_PATH.exists():
-        FULL_DATASET_PATH.unlink()
-    # Generate log and dataset files
-    customize.create_model(CHANNEL_ID, USER)
-    yield
-    # Cleanup after the tests in this module
-    if FULL_LOGS_PATH.exists():
-        FULL_LOGS_PATH.unlink()
-    if FULL_DATASET_PATH.exists():
-        FULL_DATASET_PATH.unlink()
+from .conftest import FULL_LOGS_PATH, FULL_DATASET_PATH, CHANNEL_ID, USER
 
 
 @pytest.fixture(scope="function")
